a4d538778674d16800303a4f83efb44f0271db34
keyser <keyser@033478fe-9347-4027-94cf-8a8f83edcaf9>
2015-01-02 20:41:48 +0000

    sync avec branche 2.4

--- sources/www/tftp/ajax_lib.php
+++ sources/www/tftp/ajax_lib.php
@@ -66,5 +66,10 @@ if ($_GET['mode'] == 'ping_ip') {
 	foreach ($retour as $key => $value) {
 		echo $value;
 	}
+} elseif ($_GET['mode'] == 'check_versions_pxe_client_linux') {
+	$resultat2 = exec("/usr/bin/sudo /usr/share/se3/scripts/se3_get_install_client_linux.sh 'check_version'", $retour);
+	foreach ($retour as $key => $value) {
+		echo $value;
+	}
 }
 ?>
